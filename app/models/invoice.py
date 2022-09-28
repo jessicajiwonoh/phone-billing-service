0a33d523dee5d1e761142c14a424f872bff2aa75
Jess Oh <jessicajiwonoh@gmail.com>
2022-09-28 17:05:48 -0400

    edited invoice table to include amount, date info

--- app/models/invoice.py
+++ app/models/invoice.py
@@ -3,6 +3,9 @@ from app import db
 class Invoice(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
+    amount_billed = db.Column(db.Integer, nullable=False)
+    date_billed = db.Column(db.DateTime, nullable=False)
+    date_paid = db.Column(db.DateTime)
 
 def __repr__(self):
-   return f"Invoice({self.id}, {self.customer_id})"
+   return f"Invoice({self.id}, {self.customer_id}, {self.amount_billed}, {self.date_billed}, {self.date_paid})"
